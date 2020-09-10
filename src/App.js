cfd364fd56d95d8e4184ac3b6eb2b1d1c0e39370
jhalfkan <jookz1@gmail.com>
2020-09-10 10:35:01 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { useState, useRef } from "react";
+import "./App.css";
+
+function padTime(time) {
+  return time.toString().padStart(2, "0");
+}
+
+export default function App() {
+  const [title, setTitle] = useState("Let the countdown begin !!!");
+  const [timeLeft, setTimeLeft] = useState(15 * 60);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
+
+  function startTimer() {
+    if (intervalRef.current !== null) return;
+
+    setTitle(`C'est parti!`);
+    setIsRunning(true);
+
+    intervalRef.current = setInterval(() => {
+      setTimeLeft((timeLeft) => {
+        if (timeLeft >= 1) return timeLeft - 1;
+        resetTimer();
+        return 0;
+      });
+    }, 1000);
+  }
+
+  function stopTimer() {
+    if (intervalRef.current === null) return;
+
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle("☕️ Une petite pause!");
+    setIsRunning(false);
+  }
+
+  function resetTimer() {
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle("Pret pour une nouvelle tâche?");
+    setTimeLeft(15 * 60);
+    setIsRunning(false);
+  }
+
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const seconds = padTime(timeLeft - minutes * 60);
+
+  return (
+    <div className="app">
+      <h2>{title}</h2>
+
+      <div className="timer">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+
+      <div className="buttons">
+        {!isRunning && <button onClick={startTimer}>Start</button>}
+        {isRunning && <button onClick={stopTimer}>Stop</button>}
+        <button onClick={resetTimer}>Reset</button>
+      </div>
+    </div>
+  );
+}
